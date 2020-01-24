47f1384b43425b7cb7c16dafa30051108d96b8e1
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2020-01-24 16:38:30 +0500

    fob added

---
+++ custom.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+  if (jQuery) {
+    // jQuery is loaded
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function () {
+      if ($(this).scrollTop() >= 150) {        // If page is scrolled more than 50px
+        $('#return-to-top').show(1200);    // Fade in the arrow
+      } else {
+        $('#return-to-top').hide(1200);   // Else fade out the arrow
+      }
+    });
+  } else {
+    // jQuery is not loaded
+  }
+});
