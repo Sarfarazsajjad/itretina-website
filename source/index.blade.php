8cb395a87f67ce8acfb697d6571f5be860fbb1c7
Mitretina <126672984+Mitretina@users.noreply.github.com>
2023-12-08 08:45:01 +0500

    8-12-2023

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,17 +3,18 @@
 <div id="p5-canvas-container"></div>
 <!-- Hero Section -->
 <div class="container text-center my-5">
-        <h1>Welcome to ITRetina</h1>
-        <p>Your Solution for IT Services and Medical Billing</p>
-    </div>
-    <div class="container cta-container">
-    <div class="row">
+    <h1>Welcome to ITRetina</h1>
+    <p>Your Solution for IT Services and Medical Billing</p>
+</div>
+<div class="container cta-container">
+    <div class="row justify-content-center"> <!-- Center aligning the row -->
         <!-- IT Services -->
         @include('_partials.homepage-card',['cardTitle'=>$page->homePageButtonITText,'cardText'=> $page->homePageCardITDesc,'buttonLink'=> $page->homePageButtonITLink,'buttonText'=>$page->homePageButtonITText])
-        <!--Billing Services -->
+        <!-- Billing Services -->
         @include('_partials.homepage-card',['cardTitle'=>$page->homePageButtonBillingText,'cardText'=> $page->homePageCardBillingDesc,'buttonLink'=> $page->homePageButtonBillingLink,'buttonText'=>$page->homePageButtonBillingText])
         <!-- About Us -->
-        @include('_partials.homepage-card',['cardTitle'=>$page->homePageButtonAboutUSText,'cardText'=> $page->homePageCardAbtDesc,'buttonLink'=> $page->homePageButtonAboutUsLink,'buttonText'=>$page->homePageButtonAboutUSText])
+        @include('_partials.homepage-card',['cardTitle'=>$page->homePageButtonAboutUsText,'cardText'=> $page->homePageCardAbtDesc,'buttonLink'=> $page->homePageButtonAboutUsLink,'buttonText'=>$page->homePageButtonAboutUsText])
     </div>
 </div>
 @endsection
+
--- source/pizza-app.blade.php
+++ source/pizza-app.blade.php
@@ -56,22 +56,79 @@
     <h2 class="text-center">Why Choose Our Pizza App?</h2>
 
     <div class="row">
-      <div class="col-md-4">
-        <h4>Ordering Made Effortless</h4>
-        <p>Say goodbye to the complexities of traditional order management. Our app streamlines and simplifies the
-          process, enhancing efficiency.</p>
+      <div class="col-md-4 mx-auto">
+        <h4>Branded Customer Experience</h4>
+        <p>This benefit allows you to establish and promote their brand effectively, providing a unique and consistent experience to customers.</p>
       </div>
       <div class="col-md-4">
-        <h4>Supporting Your Success</h4>
-        <p>Our customer support team is committed to ensuring a seamless experience, ready to assist with any questions
-          or concerns.</p>
+        <h4>Faster Order Processing</h4>
+        <p>The capability for restaurants to receive and process orders immediately leads to quicker order preparation and delivery, enhancing customer satisfaction and retention.</p>
       </div>
       <div class="col-md-4">
-        <h4>Advanced Technology</h4>
-        <p>Revolutionizing restaurant operations with advanced technology, our app simplifies order management and
-          boosts efficiency..</p>
+        <h4>Higher Profit Margins</h4>
+        <p>By avoiding third-party fees, you can keep the entire profit from each order, leading to higher profit margins and increased business sustainability.</p>
+      </div>
+      <div class="col-md-4 mx-auto">
+        <h4>Customer Data Ownership</h4>
+        <p>Having ownership of customer data enables you to better understand preferences and behaviors, allowing for personalized marketing efforts and improved customer engagement</p>
+      </div>
+      <div class="col-md-4 mx-auto">
+        <h4>Control Over Pricing and Menu</h4>
+        <p>The flexibility to set prices and manage menu offerings independently allows restaurants to adapt to market changes and customer demands without interference.</p>
       </div>
     </div>
+    
+  </div>
+
+  <!-- Learn More -->
+  <div class="container text-center my-5">
+      <h1>Want to know the Ingredients that Make Our Pizza App Stand Out?</h1>
+  
+      <div class="container my-5 why-choose-us-container">
+        <h2 class="text-center">FAQ's</h2>
+    
+        
+          
+            <h4>What is your online food ordering system, and how does it benefit my restaurant?</h4>
+            <p>Our online food ordering system is a robust platform designed to streamline the ordering process for restaurants. It benefits your establishment by expanding your reach to a wider customer base, increasing order accuracy, reducing operational complexities, and enhancing customer convenience, leading to improved sales and customer satisfaction.</p>
+          
+          
+            <h4>Is your system customizable to fit the unique needs of my restaurant?</h4>
+            <p>Yes, our system is highly customizable. We understand that each restaurant has unique requirements. Our platform allows for customization in menu layout, item categorization, pricing, promotions, and branding to align perfectly with your restaurant's identity and specific needs.</p>
+          
+          
+            <h4>How does the ordering process work for customers using this system?</h4>
+            <p>Customers using our system can easily browse through your restaurant's menu, select items, customize orders as desired, add to cart, and proceed to checkout seamlessly. The intuitive interface simplifies the process, ensuring a user-friendly and efficient ordering experience.</p>
+          
+          
+            <h4>What types of payment methods are supported by your system?</h4>
+            <p>Our system supports a variety of payment methods including credit/debit cards, digital wallets, and cash on delivery, offering flexibility for customers to choose their preferred payment option</p>
+          
+          
+            <h4>Can I integrate this system with my existing website or mobile app?</h4>
+            <p>Yes, our system offers seamless integration with your existing website or mobile app. This integration ensures a consistent and unified user experience across all platforms.</p>
+
+            <h4>How secure is the payment process for both my restaurant and customers?</h4>
+            <p>Security is a top priority. Our system employs robust encryption and industry-standard security protocols to ensure that all transactions are secure for both your restaurant and your customers, safeguarding sensitive information.</p>
+
+            <h4>Do you offer analytics or reporting features to track orders and customer data?</h4>
+            <p>Yes, our system provides comprehensive analytics and reporting features. Track order trends, customer preferences, sales data, and more. This data-driven insight aids in making informed business decisions.</p>
+
+            <h4>Are there any setup fees or ongoing costs associated with using your system?</h4>
+            <p>We offer flexible pricing plans tailored to your restaurant's needs. There might be setup fees depending on the plan chosen, and ongoing costs will vary based on the services and features selected.</p>
+
+            <h4>Is there a contract or commitment required to use your online ordering system?</h4>
+            <p>Yes, we usually have a service agreement outlining the terms and conditions of using our system, though the specifics can be discussed based on your preferences and requirements.</p>
+
+            <h4>How can I ensure my restaurant's branding and identity are reflected in the online ordering process?</h4>
+            <p>Our system allows for extensive customization, ensuring that your restaurant's branding, logo, color schemes, and overall identity are prominently featured throughout the ordering process, providing a consistent brand experience.</p>
+
+            <h4>Is the system compatible with different devices such as tablets, smartphones, etc.?</h4>
+            <p>Absolutely, our system is designed to be compatible with various devices, including tablets, smartphones, desktops, and more, ensuring accessibility for both your customers and your staff.</p>
+
+          
+        </div>
+
   </div>
 
   @include('_partials.footer')
