65ef32275316abff4c9728a38c392a34cec02145
Mitretina <126672984+Mitretina@users.noreply.github.com>
2023-12-12 12:23:59 +0500

    pizza app pic

---
+++ source/contact-us.blade.php
@@ -0,0 +1,47 @@
+@extends('_layouts.main')
+<head>
+
+    <title>ITRetina - Contact Us</title>
+
+</head>
+@section('body')
+
+<!-- Contact Details Section -->
+<div class="container my-5">
+      <h1 class="text-center mb-4">Reach Us At</h1>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="d-flex flex-column">
+            <div class="mb-3">
+              <h5>USA, Texas</h5>
+              <p>
+                827 Overdell Dr Sugarland TX 77479<br />
+                +18015574513
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="d-flex flex-column ">
+            <div class="mb-3">
+              <h5>Pakistan, Karachi</h5>
+              <p>
+                R-43, block 7, Gulshan-e-Johar<br />
+                +923132604692
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <h5>Email</h5>
+        <p>
+          <a href="mailto:info@itretina.com">info@itretina.com</a>
+        </p>
+      </div>
+    </div>
+
+
+<!-- Footer -->
+@include('_partials.it-footer')
+    @endsection
