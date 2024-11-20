720d512c6f62e9987e83806c770ad1d3f5486cbf
Nimra-Rajput <nimorao2001@gmail.com>
2024-11-20 13:33:12 +0500

    change-style.css-to-styles.css

--- archived/config.php
+++ archived/config.php
@@ -1,63 +1,56 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
-    'baseUrl' => '',
-    'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'production' => true,
+    // 'baseUrl' => 'https://maxsamnani.github.io/php-jigsaw',
+    //'baseUrl' => 'https://itretina.com',
+    'baseUrl' =>'',
+    'title' => 'ITRetina',
+    'year' => '2024',
+    'description' => 'IT Support Services and Software Development.',
+    'collections' => [],
+    'homePageCardITTitle' => 'IT Services',
+    'homePageCardAbtTitle' => 'About Us',
+    'homePageCardBillingTitle' => 'Billing Services',
 
-    // collections
-    'collections' => [
-        'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
-            'sort' => '-date',
-            'path' => 'blog/{filename}',
-        ],
-        'categories' => [
-            'path' => '/blog/categories/{filename}',
-            'posts' => function ($page, $allPosts) {
-                return $allPosts->filter(function ($post) use ($page) {
-                    return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
-                });
-            },
-        ],
-    ],
+    'homePageCardITDesc' => 'Custom software development, cloud services, IT support, and more',
+    'homePageCardBillingDesc' => 'Efficient and reliable billing services for hospitals, clinics, and doctors.',
+    'homePageCardAbtDesc' => 'Enabling Growth and Efficiency through innovative Solutions.',
 
-    // helpers
-    'getDate' => function ($page) {
-        return Datetime::createFromFormat('U', $page->date);
-    },
-    'getExcerpt' => function ($page, $length = 255) {
-        if ($page->excerpt) {
-            return $page->excerpt;
-        }
+    'homePageButtonITText' => 'Explore Our IT Solutions',
+    'homePageButtonBillingText' => 'Discover Our Billing Services',
+    'homePageButtonAboutUsText' => 'More About ITRetina',
 
-        $content = preg_split('/<!-- more -->/m', $page->getContent(), 2);
-        $cleaned = trim(
-            strip_tags(
-                preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content[0]),
-                '<code>'
-            )
-        );
+    'homePageButtonITLink' => 'it-services',
+    'homePageButtonBillingLink' =>  'billing-services',
+    'homePageButtonAboutUsLink' => 'about-us',
+    'dentalSetsApartBlock1Heading' => 'Experienced Virtual Client Success Managers',
+    'dentalSetsApartBlock1Desc' => 'Our team is dedicated to ensuring your needs are met. You will have a virtual success manager available 24/7, who can help you with billing assistance and answer any questions that come up while we work on your behalf.',
+    'dentalSetsApartBlock2Heading' => 'Security and Privacy',
+    'dentalSetsApartBlock2Desc' => 'Your security and privacy are our #1 priority. We protect all the patient data using an advanced encryption algorithm to prevent any theft of patient personal information or identity leaks, so you can rest easy knowing that we have got this!',
+    'dentalSetsApartBlock3Heading' => 'High Quality, Customized Solutions',
+    'dentalSetsApartBlock3Desc' => 'You just need to choose the customized plan that best fits your needs. We don’t believe in long-term contracts because we are confident that you’ll have peace of mind with us. Our Flat fee schedules allow you more flexibility and control.',
+    'dentalSetsApartBlock4Heading' => 'Organized Billing Process',
+    'dentalSetsApartBlock4Desc' => 'Our organized billing process can help dental practices streamline their billing operations, saving time and resources. This allows dentists and their staff to focus on providing quality care to their patients, rather than spending time on administrative tasks',
+    'dentalSetsApartBlock5Heading' => 'Trained Staff',
+    'dentalSetsApartBlock5Desc' => 'We specialize in complex billing processes and have a team of experienced professionals who are well-versed in the latest industry regulations and coding systems. You can be sure that your claims will be processed accurately and efficiently.',
+    'dentalFeature1Heading'=>'Dental Insurance Eligibility Verification',
+    'dentalFeature1Desc'=>'Prior verification allows you to be proactive, save time, and have confidence while delivering patient treatment plans.',
+    'dentalFeature1Icon'=>'bi-check-circle-fill',
+    'dentalFeature2Heading'=>'Dental Insurance Billing and AR/Collection',
+    'dentalFeature2Desc'=>'Our dental team is here to make it quick and convenient for you to track and collect any outstanding claims and payments from insurance companies. Get paid what you are owed.',
+    'dentalFeature2Icon'=>'bi-cash',
+    'dentalFeature3Heading'=>'Patient Follow-ups/Fill the Chair',
+    'dentalFeature3Desc'=>'Are you struggling to juggle all of your responsibilities? Let us take care of that for you. We will handle everything from appointments and follow-ups with active and inactive patients, so don’t worry about a thing!',
+    'dentalFeature3Icon'=>'bi-person-check-fill',
+    'dentalFeature4Heading'=>'Appointment Scheduling',
+    'dentalFeature4Desc'=>'>Need a more efficient way to schedule your patient appointments? Our technology schedules and streamlines new appointments in real-time and syncs them with your daily calendar.',
+    'dentalFeature4Icon'=>'bi-calendar-check-fill',
+    'dentalFeature5Heading'=>'Appointment Confirmation',
+    'dentalFeature5Desc'=>'Our team makes sure that patient appointments are confirmed and there are no last-minute cancellations. Our system follows up via phone and text before an appointment.',
+    'dentalFeature5Icon'=>'bi-telephone-forward-fill',
+    'dentalFeature6Heading'=>'Inbound and Outbound Calls',
+    'dentalFeature6Desc'=>'Whether you have 1 call per day or 100+, we will create a customized solution so you can be confident that each call is answered professionally and taken care of.',
+    'dentalFeature6Icon'=>'bi-telephone-fill',
 
-        if (count($content) > 1) {
-            return $cleaned;
-        }
-
-        $truncated = substr($cleaned, 0, $length);
-
-        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
-            $truncated .= '</code>';
-        }
-
-        return strlen($cleaned) > $length
-            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
-            : $cleaned;
-    },
-    'isActive' => function ($page, $path) {
-        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
-    },
 ];
